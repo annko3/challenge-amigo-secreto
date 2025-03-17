49b6b714d7ce3e201e0a2fffa56f8f1ef669b3da
Gigi <r.s.nicole1102@gmail.com>
2025-03-17 15:44:00 -0500

    funcion listaAmigos

--- app.js
+++ app.js
@@ -3,16 +3,25 @@
 let amigos = [];
 
 function agregarAmigo() {
-    let agregarAmigo = document.getElementById("amigo").value
+    let agregarAmigo = document.getElementById("amigo").value;
+
     if (agregarAmigo === ""){
         alert("Por favor, inserte un nombre")
     }else{
         amigos.push(agregarAmigo)
         document.getElementById("amigo").value = "";
-        console.log(amigos)
+        listaAmigos();
     }
 }
 
-function sortearAmigo() {
+function listaAmigos() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
 
+    for(let agregarAmigo of amigos) {
+        let agregaLi = document.createElement("li"); 
+        agregaLi.textContent = agregarAmigo; 
+        lista.appendChild(agregaLi)
+        
+    }
 }
